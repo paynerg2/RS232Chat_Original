8a2a7ba1eac9ee42d6fc43367af7cacb984c6ddc
Rodney Payne <paynerg2@gmail.com>
2018-10-31 00:08:08 -0400

    Add project files.

---
+++ RS232/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RS232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rtbChatEntry = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cmbSerialPorts = new System.Windows.Forms.ComboBox();
+            this.rtbReceivedText = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbChatEntry
+            // 
+            this.rtbChatEntry.Location = new System.Drawing.Point(23, 107);
+            this.rtbChatEntry.Name = "rtbChatEntry";
+            this.rtbChatEntry.Size = new System.Drawing.Size(166, 157);
+            this.rtbChatEntry.TabIndex = 0;
+            this.rtbChatEntry.Text = "";
+            this.rtbChatEntry.TextChanged += new System.EventHandler(this.rtbChatEntry_TextChanged);
+            this.rtbChatEntry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.rtbChatEntry_KeyDown);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // cmbSerialPorts
+            // 
+            this.cmbSerialPorts.FormattingEnabled = true;
+            this.cmbSerialPorts.Location = new System.Drawing.Point(222, 65);
+            this.cmbSerialPorts.Name = "cmbSerialPorts";
+            this.cmbSerialPorts.Size = new System.Drawing.Size(121, 24);
+            this.cmbSerialPorts.TabIndex = 1;
+            this.cmbSerialPorts.SelectedIndexChanged += new System.EventHandler(this.cmbSerialPorts_SelectedIndexChanged);
+            // 
+            // rtbReceivedText
+            // 
+            this.rtbReceivedText.Location = new System.Drawing.Point(213, 107);
+            this.rtbReceivedText.Name = "rtbReceivedText";
+            this.rtbReceivedText.Size = new System.Drawing.Size(166, 157);
+            this.rtbReceivedText.TabIndex = 0;
+            this.rtbReceivedText.Text = "";
+            this.rtbReceivedText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtbReceivedText_KeyPress);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(23, 294);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 369);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtbReceivedText);
+            this.Controls.Add(this.cmbSerialPorts);
+            this.Controls.Add(this.rtbChatEntry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbChatEntry;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cmbSerialPorts;
+        private System.Windows.Forms.RichTextBox rtbReceivedText;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ RS232/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms.VisualStyles;
+
+namespace RS232
+{
+    public partial class Form1 : Form
+    {
+        //Local variables
+        private string Port { get; set; }
+
+        public delegate void RS232Handler(SerialPort mySerialPort);
+
+        public event RS232Handler RS232EventOccurred;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cmbSerialPorts.Items.AddRange(ports);
+
+            //Set default selected index to the first item
+            cmbSerialPorts.SelectedIndex = 0;
+
+            Port = cmbSerialPorts.SelectedItem.ToString();
+
+            serialPort1.PortName = Port;
+
+            rtbChatEntry.Clear();
+            rtbReceivedText.Clear();
+            rtbChatEntry.Focus();
+
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //use this to invoke the event delagate and pass it the method to update the box.
+            BeginInvoke(RS232EventOccurred, serialPort1);
+
+        }
+
+        private void UpdateTextBox(SerialPort mySerialPort)
+        {
+            try
+            {
+                //serialPort1.Open();
+                string all = serialPort1.ReadExisting();
+                rtbReceivedText.Text += all;
+                rtbChatEntry.Clear();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            Run();
+        }
+        
+        private void Run()
+        {
+            
+            //Add event handler
+            RS232EventOccurred += UpdateTextBox;
+            //RS232EventOccurred += new RS232Handler(UpdateTextBox);
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            rtbChatEntry.Focus();
+
+        }
+
+        private void cmbSerialPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Port = cmbSerialPorts.SelectedItem.ToString();
+            serialPort1.PortName = Port;
+        }
+
+        private void rtbChatEntry_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void rtbChatEntry_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Proceed if an enter has been received
+            if (e.KeyValue != (char) 13) return;
+
+            string text = rtbChatEntry.Text + Environment.NewLine;
+
+            try
+            {
+                serialPort1.Write(text);
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void rtbReceivedText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
